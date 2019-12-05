4ce43a82b1047c2a59d8e7a7e31ca6c4632dc840
rmadeye <rafmadaj@gmail.com>
2019-12-05 22:08:14 +0100

    -Minor tweaks like timer

--- src/frontend.py
+++ src/frontend.py
@@ -16,43 +16,47 @@ class Front:
 
             button[i].config(state = "disabled", bg = "blue")
 
-        def get_question(id, remaining = 60):
+        def get_question(id):
 
            question = QuestionReader().read_question(id)
            question_window = Tk()
-           label_question = Label(question_window, text = question, height = 6, width = 20, font = ("Arial", 25), bg = "lightblue")
+           question_window.wm_title("Question number {}".format(id))
+           label_question = Label(question_window, text = question, height = 6, width = 55, font = ("Arial", 25), bg = "lightblue")
            label_time = Label(question_window, text = '', width = 10)
            label_question.grid(row = 0, column = 0)
+           label_time.grid(row = 1, column = 0)
 
-           if remaining <= 0:
-               label_time.configure(text="time's up!")
-           else:
-               label_time.configure(text="%d" % remaining)
-               remaining = remaining - 1
+           def countdown(count):
+               # change text in label
+               label_time['text'] = count
+
+               if count > 0:
+                   question_window.after(1000, countdown, count - 1)
+
+           countdown(60)
 
 
            question_window.mainloop()
 
-
-
-
-
+        background_image = PhotoImage("src/DB/brain_lt.png")
+        background_label = Label(window, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
         button = []
-        label_names = ['Vocabulary', 'Grammar', 'Speaking', 'Pronoun.', 'Misc']
+        label_names = ['Vocabulary - 1', 'Vocabulary - 2', 'Grammar - 1', 'Grammar - 2', 'Misc', 'Fun!']
         labels = []
         counter = 0
         columns = 0
         for i in range(25):
             button.append(Button(window, text=str((counter+ 1) * 100),
-                                 height = 6,
-                                 width = 20,
+                                 height = 7,
+                                 width = 24,
                                  command = lambda index=i: [disable(index), get_question(index)],
                                  fg = "yellow",
                                  bg = "green"))
             button[i].grid(column=columns, row=counter+1, sticky=W)
             counter += 1
             if counter % 5 == 0:
-                labels.append(Label(window, text=label_names[columns], height = 6, width = 20))
+                labels.append(Label(window, text=label_names[columns], height = 3, width = 24))
                 labels[columns].grid(column=columns, row = 0)
                 counter = 0
                 columns += 1
